47d96c5f9ea9d05f3c7949b41ebb1a32471f6cfa
Titus Fortner <titusfortner@gmail.com>
2018-04-30 08:49:27 -0400

    Fix synchronization with Explicit Wait

--- src/test/authentication/LogInTest.java
+++ src/test/authentication/LogInTest.java
@@ -4,14 +4,9 @@ import test.base.*;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-
-import java.time.Duration;
-import java.util.function.Function;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LogInTest extends Base {
 
@@ -23,16 +18,10 @@ public class LogInTest extends Base {
         String email = "user@example.com";
         String password = "password";
 
-        Wait fluentWait = new FluentWait(driver)
-                .withTimeout(Duration.ofSeconds(20))
-                .pollingEvery(Duration.ofMillis(500))
-                .ignoring(NoSuchElementException.class);
+        WebDriverWait explicitWait = new WebDriverWait(driver, 20);
 
-        WebElement emailElement = (WebElement) fluentWait.until(new Function<WebDriver, WebElement>() {
-            public WebElement apply(WebDriver driver) {
-                return driver.findElement(By.id("session_email"));
-            }
-        });
+        WebElement emailElement = explicitWait.until(
+                ExpectedConditions.presenceOfElementLocated(By.id("session_email")));
 
         emailElement.sendKeys(email);
         driver.findElement(By.id("session_password")).sendKeys(password);
