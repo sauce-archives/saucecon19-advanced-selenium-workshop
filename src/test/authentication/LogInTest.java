632c5ea8138f4b64b486cb48a199f0b4d91f0f24
Titus Fortner <titusfortner@gmail.com>
2018-04-30 08:36:55 -0400

    Fix synchronization with sleep

--- src/test/authentication/LogInTest.java
+++ src/test/authentication/LogInTest.java
@@ -8,12 +8,14 @@ import org.openqa.selenium.By;
 public class LogInTest extends Base {
 
     @Test
-    public void signInSuccessfully() {
+    public void signInSuccessfully() throws InterruptedException {
         driver.get("http://a.testaddressbook.com");
         driver.findElement(By.id("sign-in")).click();
 
         String email = "user@example.com";
         String password = "password";
+
+        Thread.sleep(2000);
 
         driver.findElement(By.id("session_email")).sendKeys(email);
         driver.findElement(By.id("session_password")).sendKeys(password);
