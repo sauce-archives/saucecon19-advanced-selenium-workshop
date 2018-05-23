62c6ff08206bc86d82ac295fd780b7f5d05e1985
Titus Fortner <titusfortner@gmail.com>
2018-05-23 13:26:05 -0400

    Actions Class Examples

--- src/test/examples/ElementActionsClass.java
+++ src/test/examples/ElementActionsClass.java
@@ -21,4 +21,23 @@ public class ElementActionsClass extends Base{
         action.click(element).build().perform();
     }
 
+    @Test
+    public void actionExamples() {
+        driver.get("http://a.testaddressbook.com");
+
+        // Actions Examples
+        Actions action = new Actions(driver);
+
+        WebElement element = driver.findElement(By.id("sign-in"));
+        WebElement element2 = driver.findElement(By.tagName("a"));
+
+        // Hover
+        action.moveToElement(element).build().perform();
+
+        // Drag & Drop
+        action.dragAndDrop(element, element2);
+
+        // click - pause - release
+        action.clickAndHold(element).pause(100).release().build().perform();
+    }
 }
