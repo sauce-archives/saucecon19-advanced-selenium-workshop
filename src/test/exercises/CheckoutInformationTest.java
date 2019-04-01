530abd3504c101c7200490afde953b5fc5649524
Titus Fortner <titusfortner@gmail.com>
2019-03-31 18:38:27 -0700

    Exercise to create test

---
+++ src/test/exercises/CheckoutInformationTest.java
@@ -0,0 +1,22 @@
+package test.exercises;
+
+import org.junit.Test;
+import test.base.Base;
+import test.data.*;
+import test.pages.*;
+
+import static org.junit.Assert.assertTrue;
+
+public class CheckoutInformationTest extends Base {
+
+    //
+    // Exercise:
+    // Arrange: Navigate to Checkout Page
+    // Act: Enter Information
+    // Assert: Successful Submission
+    //
+
+    @Test
+    public void checkout() {
+    }
+}
